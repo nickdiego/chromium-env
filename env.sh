f5444b57e1322937039075cf02ba1230f2da4c2d
Nick Diego Yamane <nick.diego@gmail.com>
2017-11-21 06:10:20 -0400

    Add initial chromium devel env dir
    Signed-off-by: Nick Diego Yamane <nick.diego@gmail.com>

---
+++ env.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# ex: ts=2 sw=4 et filetype=sh
+
+chromiumdir=$(cd `dirname $BASH_SOURCE`; pwd)
+srcdir="${chromiumdir}/src"
+builddir="out/Ozone"
+
+# Setup depot_tools
+depot="${chromiumdir}/tools/depot_tools"
+if [ -r "$depot" ]; then
+    export PATH="${depot}:$PATH"
+else
+    echo "WARNING: depot_tools dir does not exists [$depot]"
+fi
+
+# Setup ccache
+LLVM_BIN_DIR="${srcdir}/third_party/llvm-build/Release+Asserts/bin"
+export CCACHE_CPP2=yes
+export CCACHE_SLOPPINESS=time_macros
+export PATH="$LLVM_BIN_DIR:$PATH"
+
+chr_config() {
+    opts=(
+        'use_ozone=true'
+        'enable_mus=true'
+        'use_xkbcommon=true'
+        'enable_nacl=false'
+        'symbol_level=1'
+    )
+    if true; then  # TODO cmdline option?
+        opts+=( 'cc_wrapper="ccache"' )
+    fi
+
+    cmd="gn gen \"$builddir\" --args='${opts[*]}'"
+    echo "Running cmd: $cmd"
+    ( cd "$srcdir" && eval "$cmd" )
+}
+
+chr_build() {
+    target="${1:-chrome}"
+    ninja -C "$builddir" "$target"
+}
+
+chr_run() {
+    opts=(
+        '--mus'
+        '--no-sandbox'
+    )
+    cmd="${builddir}/chrome ${opts[*]}"
+    echo "Running cmd: $cmd"
+    eval "$cmd"
+}
+
